763e27dd58708757356cdedbc98bb7a233e1dfad
Eduardo Ribeiro <dudub9go@hotmail.com>
2023-11-11 13:13:56 -0300

    Utilizando um context para acessar os dados em outros componentes pela import do context

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,24 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useContext } from 'react';
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
 import { SearchForm } from './components';
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from './styles';
-import { Transaction } from '../../types/transaction';
+import { TransactionsContext } from '../../contexts/TransactionsContext';
 
 export function Transactions() {
-
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3333/transactions')
-    const data = await response.json()
-
-    setTransactions(data)
-  }
-
-  useEffect(() => {
-    loadTransactions()
-  }, []);
+ 
+    const { transactions } = useContext(TransactionsContext)
 
     return (
      <div>
