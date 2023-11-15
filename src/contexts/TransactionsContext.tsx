04909119c155ff25e0b0bc8f13310f0d744eb270
Eduardo Ribeiro <dudub9go@hotmail.com>
2023-11-14 22:33:36 -0300

    Adicionando lib axios para requisições http

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useEffect, useState } from 'react';
 import { Transaction } from '../types/transaction';
+import { api } from '../lib/axios';
 
 interface TransactionContextType {
   transactions: Transaction[];
@@ -16,16 +17,13 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
   async function fetchTransactions(query?: string) {
-    const url = new URL('http://localhost:3000/transactions');
+    const response = await api.get('transactions', {
+      params: {
+        q: query,
+      }
+    })
 
-    if (query) {
-      url.searchParams.append('q', query);
-    }
-
-    const response = await fetch(url)
-    const data = await response.json()
-
-    setTransactions(data)
+    setTransactions(response.data)
   }
 
   useEffect(() => {
